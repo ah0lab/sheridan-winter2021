ca4875cb7ca358122dfc5304fd2bb638f266538c
benjm-ma <bahola@me.com>
2021-01-26 21:38:09 -0500

    Started textbook ASPCore App Example

---
+++ prog38448/MyFirstASPCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstASPCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
