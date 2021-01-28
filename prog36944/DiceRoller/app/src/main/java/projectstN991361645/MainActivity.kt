c4433a45f0c83f1999510ed599487768d596ab1b
benjm-ma <bahola@me.com>
2021-01-28 18:54:28 -0500

    Started Lab1

---
+++ prog36944/DiceRoller/app/src/main/java/projectstN991361645/MainActivity.kt
@@ -0,0 +1,13 @@
+package project.stN991361645.org
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity () 
+{
+    override fun onCreate (savedInstanceState: Bundle?)
+    {
+        super.onCreate (savedInstanceState)
+        setContentView (R.layout.activity_main)
+    }
+}
