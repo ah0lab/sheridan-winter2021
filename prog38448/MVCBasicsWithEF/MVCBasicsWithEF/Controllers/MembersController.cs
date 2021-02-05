c16ee431c45491713dbbf948f58b3c47152d1e73
Ben <aholab@sheridancollege.ca>
2021-02-05 12:06:39 -0500

    Completed W3 Walktrhough

--- prog38448/MVCBasicsWithEF/MVCBasicsWithEF/Controllers/MembersController.cs
+++ prog38448/MVCBasicsWithEF/MVCBasicsWithEF/Controllers/MembersController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -16,9 +17,89 @@ namespace MVCBasicsWithEF.Controllers
             _context = context;
         }
 
+        private bool MemberExists(int id)
+            => _context.Members.Any(e => e.Id == id);
+
         public async Task<IActionResult> Index()
         {
             return View (await _context.Members.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (member == null) return NotFound();
+
+            return View(member);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var member = await _context.Members.FindAsync(id);
+
+            if (member == null) return NotFound();
+
+            return View(member);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,
+            [Bind("Id,FirstName,LastName")] Member member)
+        {
+            if (id != member.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(member);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExists(member.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(member);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+            _context.Members.Remove(member);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (member == null) return NotFound();
+
+            return View(member);
         }
 
         [HttpPost]
